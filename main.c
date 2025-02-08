681371bb509c8c741fe910790bffd5d6617b5262
= <=>
2025-02-08 18:51:24 +0100

    Implementata gestione degli stati del gioco con inizio partita, gestione dei turni in game, fine partita e nuova partita. Avviata prima implementazione per stampare campo su terminale e controllare stato di vittoria, sconfitta o pareggio.

--- main.c
+++ main.c
@@ -6,11 +6,16 @@
 
 #define SIZE 3
 
-int random_max_min(int, int);
-void enemyGame(char campo[][SIZE]);		//
-void playerGame(char campo[][SIZE]);	//
-void clearField(char campo[][SIZE]);		// clear the field so empty the array
-void printField(void); 					// print the field to show it graphically
+enum status {WON, LOST, DRAW, NEWGAME, RUNNING}; 
+enum players {PLAYER, ENEMY};
+
+int random_max_min(int, int); 			// function for random number generator
+void game(); // a single game
+void enemyGame(char campo[][SIZE], enum players *);		// 
+void playerGame(char campo[][SIZE], enum players *);	//
+void checkVictory(); 					// function that checks if anyone won
+void clearField(char campo[][SIZE]);	// clear the field so empty the array
+void printField(char campo[][SIZE]); 	// print the field to show it graphically
 
 
 // ============================================================================= //
@@ -21,15 +26,39 @@ void printField(void); 					// print the field to show it graphically
 
 int main(void)
 {
+	
+	enum status gameStatus = NEWGAME; // as soon you start the program we can assume we're starting a new game
+	enum players selectedPlayer; // stores which player is playing
+
 	srand(time(NULL));
 
-	char campo[SIZE][SIZE]; // definisco il campo come un array di char dove posiziono 'X' e 'O'
+	char campo[SIZE][SIZE]; // define the field as a char array assigning 'X' and 'O'
 	
 	clearField(campo); // clear the field
-	//printField();
-	// choose first player 
-   	//playerGame();
-   	//enemyGame();
+	printField(campo); // print the field in the terminal
+	
+	// choose first player, if new game then select player based on random number
+	if(random_max_min(0, 1) && gameStatus == NEWGAME) 
+	{
+		selectedPlayer = PLAYER; // start player1
+	}  
+	else selectedPlayer = ENEMY; // start player2
+
+	gameStatus = RUNNING; // after i selected the player we can consider the game is running
+
+	// players can make a game until no one win
+	while(gameStatus == RUNNING) 
+	{
+		// one of the two players makes a game 
+		if(selectedPlayer == PLAYER)
+		{
+			playerGame(campo, &selectedPlayer);
+		}
+		else enemyGame(campo, &selectedPlayer);
+		// printField();
+		// checkVictory();
+	}
+   	
 }
 
 // ============================================================================= //
@@ -38,8 +67,38 @@ int main(void)
 // ============================================================================= //
 // ============================================================================= //
 
+void game()
+{
+	/*
+	clearField(campo); // clear the field
+	printField(campo); // print the field in the terminal
+	
+	// choose first player, if new game then select player based on random number
+	if(random_max_min(0, 1) && gameStatus == NEWGAME) 
+	{
+		selectedPlayer = PLAYER; // start player1
+	}  
+	else selectedPlayer = ENEMY; // start player2
 
-void enemyGame(char campo[][SIZE])
+	gameStatus = RUNNING; // after i selected the player we can consider the game is running
+
+	// players can make a game until no one win
+	while(gameStatus == RUNNING) 
+	{
+		// one of the two players makes a game 
+		if(selectedPlayer == PLAYER)
+		{
+			playerGame(campo, &selectedPlayer);
+		}
+		else enemyGame(campo, &selectedPlayer);
+		// printField();
+		// checkVictory();
+	}
+	*/
+}
+
+
+void enemyGame(char campo[][SIZE], enum players *selectedPlayer)
 {
 	// generate random coordinates between 0 and 2
 	short x = random_max_min(0, 2); // (short)
@@ -52,10 +111,12 @@ void enemyGame(char campo[][SIZE])
 	}
 
 	campo[x][y] = 'O'; // insert enemy player sign
+
+	*selectedPlayer = PLAYER;
 }
 
 
-void playerGame(char campo[][SIZE])
+void playerGame(char campo[][SIZE], enum players *selectedPlayer)
 {
 	short x, y; // coordinates
 	do
@@ -66,11 +127,20 @@ void playerGame(char campo[][SIZE])
 		scanf("%hd", &y);
 	}
 	// field must be empty and coordinates between 0 and 2
-	while( (x<0 || x>2) && (y<0 || y>2) && (campo[x][y] != '\0') ); 
+	while( (x<0 || x>2) && (y<0 || y>2) && (campo[x][y] != '\0') );
 	
-
 	campo[x][y] = 'X'; // insert player sign
+	
+	*selectedPlayer = ENEMY;
+}
 
+void checkVictory()
+{
+	// controlla se qualcuno ha fatto tris
+	// se player ha fatto allora gameStatus = WON, se enemy allora gameStatus = LOST 
+	// altrimenti se campo e' tutto pieno e nessuno ha vinto allora gameStatus = DRAW	
+	// chiedi se iniziare nuova partita o uscire, 
+	// se avvio nuova partita fai game(); e game() include tutto il main da cleanField() alla fine
 }
 
 
@@ -89,9 +159,13 @@ void clearField(char campo[][SIZE])
 
 
 // print the field on the cli
-void printField(void)
+void printField(char campo[][SIZE])
 {
-
+	printf(" %c | %c | %c\n", campo[0][0], campo[0][1], campo[0][2]);
+	printf("%s\n", "---+---+---");
+	printf(" %c | %c | %c\n", campo[1][0], campo[1][1], campo[1][2]);
+	printf("%s\n", "---+---+---");
+	printf(" %c | %c | %c\n", campo[2][0], campo[2][1], campo[2][2]);
 }
 
 
@@ -101,14 +175,6 @@ int random_max_min(int min, int max){
 }
 
 
-
-
-
-
-// funzione per generare segno O casuale da parte dell'avversario
-// funzione per far inserire al player il segno X
-// funzione per stampare grafica del campo
-// funzione per pulire campo e avviare nuova partita
 
 /*
 
